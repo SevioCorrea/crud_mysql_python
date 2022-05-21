e96183129e6f5ad55821e98924eff46d61825c57
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-05-21 10:33:45 -0300

    CRUD em Python acessando Banco de Dados MySQL.

--- CRUD.py
+++ CRUD.py
@@ -3,7 +3,7 @@ import mysql.connector
 conexao = mysql.connector.connect(
     host='localhost',
     user = 'root',
-    password = 'campeao4',
+    password = '12345',
     database = 'practice',
 )
 cursor = conexao.cursor()
@@ -13,12 +13,12 @@ cursor = conexao.cursor()
 # CREATE
 nome_produto = "produto1"
 valor = 15
-comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+comando = f'INSERT INTO bdpractice (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
 cursor.execute(comando)
 conexao.commit() # Para editar o Banco de Dados
 
 # READ
-comando = f'SELECT * FROM vendas'
+comando = f'SELECT * FROM bdpractice'
 cursor.execute(comando)
 resultado = cursor.fetchall() # Para ler o Banco de Dados
 print (resultado)
@@ -26,14 +26,14 @@ print (resultado)
 # UPDATE
 nome_produto = "produto2"
 valor = 6
-comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+comando = f'UPDATE bdpractice SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
 cursor.execute(comando)
 conexao.commit() # Para editar o Banco de Dados
 
 # DELETE
 nome_produto = "produto2"
-comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
-ursor.execute(comando)
+comando = f'DELETE FROM bdpractice WHERE nome_produto = "{nome_produto}"'
+cursor.execute(comando)
 conexao.commit() # Para editar o Banco de Dados
 
 cursor.close()
